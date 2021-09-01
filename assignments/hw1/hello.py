07157d327b74542af7ead7f736178e251497f60b
Erin Phillips <phillipsen@g.cofc.edu>
2021-09-01 17:12:38 -0400

    completed hw1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,14 @@
+"""
+Name: Erin Phillips
+hello.py
+
+Problem: This program prints "Hello world" to the screen.
+
+Certificate of Authenticity
+I certify that this assignment is entirely my own work.
+"""
+
+def main():
+    print("hello, world!")
+
+main()
