c073db604fb29ec32189e8d91c4cdecf40eb6b7c
Erin Phillips <phillipsen@g.cofc.edu>
2021-09-08 14:38:09 -0400

    completed lab 2

--- labs/lab2/lab2.py
+++ labs/lab2/lab2.py
@@ -1,4 +1,66 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Erin Phillips
+lab2.py
 """
+
+import math
+
+
+def sum_of_threes():
+    upper_bound = eval(input("Enter the upper bound: "))
+    acc = 0
+    for i in range(0, upper_bound + 1, 3):
+        acc = acc + i
+
+    print(acc)
+
+
+sum_of_threes()
+
+
+def multiplication_table():
+    for h in range(1, 11):
+        for l in range(1, 11):
+            print(h * l, end=" ")
+        print()
+
+
+multiplication_table()
+
+
+def triangle_area():
+    a = eval(input("Enter the length of side a: "))
+    b = eval(input("Enter the length of side b: "))
+    c = eval(input("Enter the length of side c: "))
+    s = (a + b + c) / 2
+    area = math.sqrt(s * (s-a) * (s-b) * (s-c))
+    print(area)
+
+
+triangle_area()
+
+
+def sum_squares():
+    start = eval(input("Enter the start: "))
+    end = eval(input("Enter the end: "))
+    acc = 0
+    for i in range(start, end + 1):
+        acc = acc + i ** 2
+
+    print(acc)
+
+
+sum_squares()
+
+
+def power():
+    base = eval(input("Enter the base: "))
+    exp = eval(input("Enter the exponent: "))
+    acc = 1
+    for i in range(exp):
+        acc = acc * base
+
+    print(acc)
+
+
+power()
