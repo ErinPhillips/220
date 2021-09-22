24a88a409ab608854e8628a64fca539d8211fcd0
Erin Phillips <phillipsen@g.cofc.edu>
2021-09-22 15:22:51 -0400

    completed lab 4

--- labs/lab4/lab4.py
+++ labs/lab4/lab4.py
@@ -1,55 +1,36 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Erin Phillips
+lab4.py
 """
 
+from math import *
 from graphics import *
 
 
 def squares():
-    """  <---  You can use tripled quotes to write a multi-line comment....
-
-    Modify the following function to:
-
-    Draw squares (20 X 20) instead of circles. Make sure that the center of the square
-    is at the point where the user clicks. Make the window 400 by 400.
-
-    Have each successive click draw an additional square on the screen (rather
-    than just moving the existing one).
-
-    Display a message on the window "Click again to quit" after the loop, and
-    wait for a final click before closing the window.
-    """
-    # Creates a graphical window
     width = 400
     height = 400
     win = GraphWin("Lab 4", width, height)
 
-    # number of times user can move circle
     num_clicks = 5
 
-    # create a space to instruct user
     inst_pt = Point(width / 2, height - 10)
-    instructions = Text(inst_pt, "Click to move circle")
+    instructions = Text(inst_pt, "Click to move square")
     instructions.draw(win)
 
-    # builds a circle
-    shape = Circle(Point(50, 50), 20)
-    shape.setOutline("red")
-    shape.setFill("red")
+    shape = Rectangle(Point(50, 50), Point(70, 70))
+    shape.setOutline("pink")
+    shape.setFill("pink")
     shape.draw(win)
 
-    # allows the user to click multiple times to move the circle
     for i in range(num_clicks):
         p = win.getMouse()
-        c = shape.getCenter()  # center of circle
+        shape = Rectangle(Point(p.getX() - 10, p.getY() - 10), Point(p.getX() + 10, p.getY() + 10))
+        shape.setOutline("pink")
+        shape.setFill("pink")
+        shape.draw(win)
 
-        # move amount is distance from center of circle to the
-        # point where the user clicked
-        dx = p.getX() - c.getX()
-        dy = p.getY() - c.getY()
-        shape.move(dx, dy)
-
+    instructions.setText("Click again to quit")
     win.getMouse()
     win.close()
 
@@ -59,17 +40,90 @@ def rectangle():
     This program displays information about a rectangle drawn by the user.
     Input: Two mouse clicks for the opposite corners of a rectangle.
     Output: Draw the rectangle.
-         Print the perimeter and area of the rectangle.
+         Display the perimeter and area of the rectangle.
     Formulas: area = (length)(width)   and    perimeter = 2(length+width)
     """
-    pass
+
+    width = 400
+    height = 400
+    win = GraphWin("Rectangle", width, height)
+
+    inst_pt = Point(width / 2, height - 10)
+    instructions = Text(inst_pt, "Click the opposite corners of the rectangle")
+    instructions.draw(win)
+
+    p1 = win.getMouse()
+    p2 = win.getMouse()
+    shape = Rectangle(p1, p2)
+    shape.setOutline("pink")
+    shape.setFill("pink")
+    shape.draw(win)
+
+    rect_length = abs(p1.getX() - p2.getX())
+    rect_width = abs(p1.getY() - p2.getY())
+    area = rect_length * rect_width
+    perimeter = (rect_length * 2) + (rect_width * 2)
+
+    area_pt = Point(width / 2, height - 30)
+    area_text = Text(area_pt, "The area is: " + str(area))
+    area_text.draw(win)
+
+    perimeter_pt = Point(width / 2, height - 50)
+    perimeter_text = Text(perimeter_pt, "The perimeter is: " + str(perimeter))
+    perimeter_text.draw(win)
+
+    instructions.undraw()
+    instructions.setText("Click to quit")
+    instructions.draw(win)
+
+    win.getMouse()
+    win.close()
+
+rectangle()
 
 
-def main():
-    squares()
-    # rectangle()
-    # circle()
-    # pi2()
+def circle():
+    width = 400
+    height = 400
+    win = GraphWin("Circle", width, height)
+
+    p1 = win.getMouse()
+    p2 = win.getMouse()
+    x1 = p1.getX()
+    x2 = p2.getX()
+    y1 = p1.getY()
+    y2 = p2.getY()
+    radius = ((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    radius = sqrt(radius)
+    shape = Circle(p1, radius)
+    shape.setOutline("pink")
+    shape.setFill("pink")
+    shape.draw(win)
+
+    radius_pt = Point(width / 2, height - 25)
+    radius_text = Text(radius_pt, "The radius is: " + str(radius))
+    radius_text.draw(win)
+
+    instructions_pt = Point(width / 2, height - 50)
+    instructions_text = Text(instructions_pt, "Click to quit")
+    instructions_text.draw(win)
+
+    win.getMouse()
+    win.close()
 
 
-main()
+circle()
+
+
+def pi2():
+    n = eval(input("Enter the number of terms to sum: "))
+    acc = 0
+    for i in range(n):
+        num = 4
+        denom = 1 + 2 * i
+        frac = (num / denom) * ((-1) ** i)
+        acc = acc + frac
+    print(pi - acc)
+
+
+pi2()
